c59e2b802dbc1e8adc88e9f3c3fdfeb62f606b4f
Hasansarwer <hasanmathju@gmail.com>
2024-12-29 01:11:52 +0600

    mongodb details chenged

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:pFFxgDVM9MP6UA6pqr4z2oLu@172.21.186.225:27017";
+const mongoURI =  "mongodb://root:8n2hZQosCndcfEMVhsTROty3@172.21.117.161:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
