fd0e633cebb4a52a26f6e67ba52e653236f9d016
Hasansarwer <hasanmathju@gmail.com>
2024-12-29 17:51:50 +0600

    Appoinment form complete

--- src/Components/AppointmentForm/AppointmentForm.jsx
+++ src/Components/AppointmentForm/AppointmentForm.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
     const [name, setName] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
+    const [date, setDate] = useState('');
     const [selectedSlot, setSelectedSlot] = useState(null);
   
     const handleSlotSelection = (slot) => {
@@ -38,6 +39,26 @@ const AppointmentForm = ({ doctorName, doctorSpeciality, onSubmit }) => {
             required
           />
         </div>
+        <div className="form-group">
+          <label htmlFor="date">Date of Appoinment:</label>
+          <input 
+          type="date"
+          id="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          required
+          /></div>
+        <div className="form-group">
+          <label htmlFor="selectSlot">Book Time Slot:</label>
+          <input 
+          type="time" 
+          id="selectSlot"
+          min="09:00"
+          max="17:00"
+          value={selectedSlot}
+          onChange={(e) => setSelectedSlot(e.target.value)}
+          required
+          /></div>
         <button type="submit">Book Now</button>
       </form>
     );
