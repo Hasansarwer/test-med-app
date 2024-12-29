0398ac1c8b357350b2d56b8a34373ed30048ea4d
Hasansarwer <hasanmathju@gmail.com>
2024-12-29 16:27:44 +0600

    Instant consultent booking incorporated successfully

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Landing_Page from './Components/Landing_Page/Landing_Page'
 import Login from './Components/Login/Login'
 import Sign_Up from './Components/Sign_Up/Sign_Up'
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation'
 
 function App() {
 
@@ -15,6 +16,8 @@ function App() {
           <Route path="/" element={<Landing_Page />} />
           <Route path="/login" element={<Login />} />
           <Route path='/signup' element={<Sign_Up />} />
+          <Route path='/instant-consultation' element= {<InstantConsultation />} />
+          <Route path='*' element={<h1>404 Not Found</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
