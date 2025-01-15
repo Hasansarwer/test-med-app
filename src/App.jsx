4e2ef2a7f2d1d5f088e297d63064829dee33fc9e
Hasansarwer <hasanmathju@gmail.com>
2025-01-15 22:51:03 +0600

    Give Review complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Login from './Components/Login/Login'
 import Sign_Up from './Components/Sign_Up/Sign_Up'
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation'
 import Notifications from './Components/Notification/Notification'
+import ReviewForm from './Components/ReviewForm/ReviewForm'
+import GiveReviews from './Components/GiveReviews/GiveReviews'
 function App() {
 
   return (
@@ -17,6 +19,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path='/signup' element={<Sign_Up />} />
           <Route path='/instant-consultation' element= {<InstantConsultation />} />
+          <Route path='/review' element = {<ReviewForm />} />
+          <Route path='/giveReview/:id' element = {<GiveReviews />} />
           <Route path='*' element={<h1>404 Not Found</h1>} />
         </Routes>
       </BrowserRouter>
